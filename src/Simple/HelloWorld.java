5b64606eca1ad460c04fefcc3407be063b013ad3
opchance69 <124510938+opchance69@users.noreply.github.com>
2023-02-05 08:33:00 -0800

    extra strings

--- src/Simple/HelloWorld.java
+++ src/Simple/HelloWorld.java
@@ -7,21 +7,51 @@ public class HelloWorld {
 	}
 
 	public static void main(String[] args) {
-		
-	    String message = "Tings n stuff";
-	    System.out.println(message);
-	    message = "boobies";
-	    System.out.println(message);
-	    String otherMessage = "tits";
-	    System.out.println(message + " " + otherMessage);
-	    // TODO Auto-generated method stub
-		//System.out.println("Hi");
+
+		String message = "Tings n stuff";
+		System.out.println(message);
+		message = "boobies";
+		System.out.println(message);
+		String otherMessage = "tits";
+		System.out.println(message + " " + otherMessage);
+		// TODO Auto-generated method stub
+		// System.out.println("Hi");
 		System.out.println("I");
-		/*System.out.println("Got");
-		System.out.println("A");
-		System.out.println("BIG");*/
-	    System.out.println("Boob Tits");
+		/*
+		 * System.out.println("Got"); System.out.println("A");
+		 * System.out.println("BIG");
+		 */
+		System.out.println("Boob Tits");
 		
+	    int counter = 0;
+	    System.out.println("Counter set at: " + counter);
+		counter = counter + 1;
+		System.out.println("Counter set at: " + counter);
+		counter = counter + 2;
+	    System.out.println("Counter set at: " + counter);
+	    counter = counter + 2;
+	    counter = counter + 1;
+		System.out.println("Counter set at: " + counter);
+	    
+	    counter = 5 ;
+	    System.out.println("Counter set at: " + counter);
+	    
+	    int spooky = 69;
+	    System.out.println("Batman; " + spooky + counter);
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
+	    
 	}
 
-} 
+}
