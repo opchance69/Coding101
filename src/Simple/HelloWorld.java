cb9c444fb08ed99241dfc3ac029b0ed37205270b
opchance69 <124510938+opchance69@users.noreply.github.com>
2023-02-05 07:47:08 -0800

    initial

---
+++ src/Simple/HelloWorld.java
@@ -0,0 +1,20 @@
+package Simple;
+
+public class HelloWorld {
+
+	public HelloWorld() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//System.out.println("Hi");
+		System.out.println("I");
+		/*System.out.println("Got");
+		System.out.println("A");
+		System.out.println("BIG");*/
+		System.out.println("Sock");
+		
+	}
+
+}
